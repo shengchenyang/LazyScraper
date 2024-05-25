c6111133908f31ecf6f2db908861d214169944cc
shengchenyang <15538221825@163.com>
2024-05-25 18:58:22 +0800

    feat: add cookiecutter hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+REMOVE_PATHS = []
+
+source_control = "{{ cookiecutter.source_control }}"
+if source_control == "github":
+    REMOVE_PATHS.append(".gitlab")
+    REMOVE_PATHS.append(".gitlab-ci.yml")
+elif source_control == "gitlab":
+    REMOVE_PATHS.append(".github")
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
